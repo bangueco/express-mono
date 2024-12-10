36cfc9470aa97972434a29afb6337cdcb8c54263
bangueco <121280104+bangueco@users.noreply.github.com>
2024-12-10 22:31:41 +0800

    feat: add user service

---
+++ backend/src/services/userService.ts
@@ -0,0 +1,41 @@
+import bcrypt from "@lib/bcrypt";
+import { ApiError, ValidationError } from "@lib/utils/appError";
+import httpStatusCode from "@lib/utils/httpStatusCode";
+import { User } from "@prisma/client";
+import userRepository from "@repositories/userRepository";
+
+const findById = async (userId: number) => {
+  const user = await userRepository.findById(userId);
+
+  if (!user) {
+    throw new ApiError(httpStatusCode.NOT_FOUND, "User not found!");
+  }
+
+  return user;
+};
+
+const createUser = async (firstName: string, lastName: string,
+  email: string, password: string
+) => {
+  const existingEmail = await userRepository.findByEmail(email);
+
+  if (existingEmail) {
+    throw new ValidationError(httpStatusCode.BAD_REQUEST, "email", "Email is already taken!");
+  }
+
+  const hashedPassword = bcrypt.hashPassword(password);
+
+  return await userRepository.createUser(firstName, lastName, email, hashedPassword);
+};
+
+const updateUser = async (userId: number, userData: Partial<User>) => {
+  return await userRepository.updateUser(userId, userData);
+};
+
+const deleteUser = async (userId: number) => {
+  return await userRepository.deleteUser(userId);
+};
+
+export default {
+  findById, createUser, updateUser, deleteUser
+};
