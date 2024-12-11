808a2368aec9ba0d6c02793e46c4893feea814ee
bangueco <121280104+bangueco@users.noreply.github.com>
2024-12-11 16:54:26 +0800

    feat: add middlewares and register routes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,41 @@
 import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import authRouter from "@routes/authRoute";
+import errorHandler from "@middlewares/errorHandler";
+import unknownEndpoint from "@middlewares/unknownEndpoint";
 
 const app = express();
+
+// parse json body request
+app.use(express.json());
+
+// parse urlencoded request body
+app.use(express.urlencoded({extended: true}));
+
+// enable cors
+app.use(cors());
+
+// set security HTTP headers
+app.use(helmet());
+
+// set rate limiting for endpoints
+app.use(rateLimit({windowMs: 15 * 60 * 1000, max: 100}));
+
+// parse cookies
+app.use(cookieParser());
+
+// gzip compression
+app.use(compression());
+
+// Routes
+app.use("/api/auth", authRouter);
+
+// Error handler middlewares
+app.use(unknownEndpoint);
+app.use(errorHandler);
 
 export default app;
