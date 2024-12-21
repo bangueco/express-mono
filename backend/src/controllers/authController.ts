76ea9e362b1a6d99433073597c50df824874a120
bangueco <121280104+bangueco@users.noreply.github.com>
2024-12-21 12:33:42 +0800

    refactor: small code improvements

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -2,21 +2,21 @@ import httpStatusCode from "@lib/utils/httpStatusCode";
 import authService from "@services/authService";
 import { NextFunction, Request, Response } from "express";
 
-interface RegisterRequest {
+interface IRegisterRequest {
   firstName: string;
   lastName: string;
   email: string;
   password: string;
 }
 
-interface LoginRequest {
+interface ILoginRequest {
   email: string;
   password: string;
 }
 
 const register = async (request: Request, response: Response, next: NextFunction): Promise<void> => {
   try {
-    const { firstName, lastName, email, password } = request.body as RegisterRequest;
+    const { firstName, lastName, email, password } = request.body as IRegisterRequest;
 
     const { accessToken, user } = await authService.register(firstName, lastName, email, password);
 
@@ -25,16 +25,14 @@ const register = async (request: Request, response: Response, next: NextFunction
       user,
       token: accessToken
     });
-
-    return;
   } catch (error) {
     return next(error);
   }
 };
 
-const login = async (request: Request, response: Response, next: NextFunction) => {
+const login = async (request: Request, response: Response, next: NextFunction): Promise<void> => {
   try {
-    const { email, password } = request.body as LoginRequest;
+    const { email, password } = request.body as ILoginRequest;
 
     const { accessToken, user } = await authService.login(email, password);
 
@@ -43,8 +41,6 @@ const login = async (request: Request, response: Response, next: NextFunction) =
       user,
       token: accessToken
     });
-
-    return;
   } catch (error) {
     return next(error);
   }
--- backend/src/lib/utils/config.ts
+++ backend/src/lib/utils/config.ts
@@ -2,27 +2,24 @@ import dotenv from "dotenv";
 import logger from "./logger";
 dotenv.config();
 
-const databaseUrl = process.env.DATABASE_URL;
-const environment = process.env.NODE_ENV;
-const accessTokenKey = process.env.ACCESS_TOKEN_KEY;
-const refreshTokenKey = process.env.REFRESH_TOKEN_KEY;
+const { DATABASE_URL, NODE_ENV, ACCESS_TOKEN_KEY, REFRESH_TOKEN_KEY } = process.env
 
-if (!databaseUrl) {
+if (!DATABASE_URL) {
   logger.error("DATABASE_URL environment variable is not defined!");
   throw new Error("DATABASE_URL environment variable is not defined!");
 }
 
-if (!environment) {
+if (!NODE_ENV) {
   logger.error("NODE_ENV environment variable is not defined!");
   throw new Error("NODE_ENV environment variable is not defined!");
 }
 
-if (!accessTokenKey) {
+if (!ACCESS_TOKEN_KEY) {
   logger.error("ACCESS_TOKEN_KEY environment variable is not defined!");
   throw new Error("ACCESS_TOKEN_KEY environment variable is not defined!");
 }
 
-if (!refreshTokenKey) {
+if (!REFRESH_TOKEN_KEY) {
   logger.error("REFRESH_TOKEN_KEY environment variable is not defined!");
   throw new Error("REFRESH_TOKEN_KEY environment variable is not defined!");
 }
@@ -35,10 +32,10 @@ const config: {
   refreshTokenKey: string
 } = {
   port: Number(process.env.PORT) || 3000,
-  databaseUrl,
-  environment,
-  accessTokenKey,
-  refreshTokenKey
+  databaseUrl: DATABASE_URL,
+  environment: NODE_ENV,
+  accessTokenKey: ACCESS_TOKEN_KEY,
+  refreshTokenKey: REFRESH_TOKEN_KEY
 };
 
 export default config;
