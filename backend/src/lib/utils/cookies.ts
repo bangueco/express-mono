409cea7b5bd60817df74326b34ea5962fac21639
bangueco <121280104+bangueco@users.noreply.github.com>
2024-12-30 12:49:48 +0800

    feat: add cookie util

---
+++ backend/src/lib/utils/cookies.ts
@@ -0,0 +1,27 @@
+import config from "./config";
+
+type CookieOptions = {
+  httpOnly: boolean;
+  maxAge: number;
+  secure: boolean;
+  sameSite: "strict" | "none" | "lax";
+};
+
+type CookieConfig = {
+  [key: string]: {
+    name: string;
+    options: CookieOptions;
+  };
+};
+
+export const cookieConfig: CookieConfig = {
+  refreshToken: {
+    name: "refreshToken",
+    options: {
+      httpOnly: true,
+      maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+      secure: config.environment === "production",
+      sameSite: config.environment === "production" ? "strict" : "none",
+    },
+  },
+};
